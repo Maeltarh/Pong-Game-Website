5b37d1587d16bdf4f64a5070759b2af72002aedc
merlin metzger <mmetzger@k2r4p8.42mulhouse.fr>
2023-12-21 22:25:33 +0100

    empty

--- backend/dist/api42.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class Api42Service {
-    private httpService;
-    private readonly BASE_URL;
-    constructor(httpService: HttpService);
-    getAccessTokenFromCode(code: string): Promise<string>;
-    getUserInfo(token: string): Promise<any>;
-}
